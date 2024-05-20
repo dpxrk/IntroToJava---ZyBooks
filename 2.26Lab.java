681c72b28437a022eb3d80f15e807a8cec7a7696
dpxrk <danielpark0503@gmail.com>
2024-05-20 18:30:39 -0400

    2.26

---
+++ 2.26Lab.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LabProgram {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      
+      int age;
+      int lbs;
+      int bpm;
+      int minutes;
+      
+      double calories;
+      
+      age = scnr.nextInt();
+      lbs = scnr.nextInt();
+      bpm = scnr.nextInt();
+      minutes = scnr.nextInt();
+      
+      calories = (double)((age * 0.2757) + (lbs * 0.03295) + (bpm * 1.0781) - 75.4991) * minutes / 8.368;
+      
+      System.out.print("Calories: ");
+      System.out.printf("%.2f", calories);
+      System.out.println(" calories"); 
+   }
+} 
